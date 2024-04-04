3c5437e03d9989e4790dddb948eda6d6d5e8103b
AlbertoLutz <albertolutzdias@gmail.com>
2024-04-04 00:03:00 -0300

    Iniciando projeto Java

---
+++ src/main/java/lutz/com/passin/PassInApplication.java
@@ -0,0 +1,13 @@
+package lutz.com.passin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassInApplication.class, args);
+	}
+
+}
